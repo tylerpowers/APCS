c5291b2236291a1316643aa5fbee5ccb30d09d14
tyler <tylerpowers4@gmail.com>
2020-09-03 10:56:03 -0400

    Initial commit

---
+++ src/MyFirstClass.java
@@ -0,0 +1,7 @@
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("Dark Theme is for cool kids");
+        String name = "Tyler";
+        System.out.printf("My name is %s", name);
+    }
+}
